9a786f8a1a31f05cb0cba5d613ece516a5ce9765
EllyKiplaa <ellykiplangat200@gmail.com>
2020-06-28 16:50:09 +0300

    Save a new user and append it

--- user.py
+++ user.py
@@ -4,8 +4,18 @@ class User:
     
     '''
     user_info = []
+def __init__(self, username, password):
+    '''
+    defining the object properties
+    '''    
+
+    self.username = username
+    self.password = password
 
 def save_info(self):
     '''
-    this method will save information into the empty user_info array
+    this method will save infomation of the user
     '''
+    User.user_info.append(self)
+
+    

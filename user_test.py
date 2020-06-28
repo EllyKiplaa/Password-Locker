d082c67c97c1cb48d7c7bf2feea1c205e7e04a6f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-06-28 16:48:53 +0300

    Save uer info then save multiple user infos

--- user_test.py
+++ user_test.py
@@ -1,9 +1,11 @@
-import unnitest
+import unittest # Importing the unittest module
 from user import User
 
-class TestContact(unittest.TestCase):
+
+
+class TestUser(unittest.TestCase):
         '''
-        Test class that defines test cases for the user class behaviours
+        Test class that defines test cases for the user class behaviors
 
 
         Args:
@@ -17,13 +19,35 @@ def setUp(self):
         this  Set up method is set to run before each test cases.
     
         '''
-        self.new_contact = User("Eli","20088") # create user object
+        self.new_user = User("Eli","20088") # create user object
 
 def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_username.username,"James")
-        self.assertEqual(self.new_password.password,"james@ms.com")
+        self.assertEqual(self.new_username.username,"Eli")
+        self.assertEqual(self.new_password.password,"20088")
+
+def test_save_user(self):
+        '''
+        test_save_user test case to test if the User object is saved into
+        the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_info),1)
+
+def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_info
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","Eli","20088") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_info),2) 
+
+
+if __name__ == '__main__':
+    unittest.main()
     

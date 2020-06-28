943f701531bd64d554637fe493ac078ab9a342bf
EllyKiplaa <ellykiplangat200@gmail.com>
2020-06-28 19:35:23 +0300

    Add a test case that will delete the user from our user info

--- user_test.py
+++ user_test.py
@@ -54,6 +54,16 @@ def test_save_multiple_user(self):
         test_user.save_user()
         self.assertEqual(len(User.user_info),2) 
 
+def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user info
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","Peter","1234") # new 
+        test_user.save_user()
+
+        self.new_user.delete_user()# Deleting a user object
+        self.assertEqual(len(User.user_info),1)
 
 
 
